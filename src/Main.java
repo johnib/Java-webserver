949512746216274a1a2090105808b8583906f1a2
Jonathan Rubin Yaniv <johnitravolta@gmail.com>
2015-11-30 20:22:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jonathan Yaniv on 30/11/2015.
+ * Copyright (c) 2015 Jonathan Yaniv. All rights reserved.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
