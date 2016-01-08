301a8bab7b24c4bc1240bbba2a1cea472a2d2ab9
Nitsan Bracha <nitsi.br@gmail.com>
2016-01-08 22:52:50 +0200

    changing RunnableDownloader adding FileUtills.java

--- src/RunnableDownloader.java
+++ src/RunnableDownloader.java
@@ -1,19 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 /**
  * Created by Jonathan Rubin Yaniv and Nitsan Bracha on 08/01/2016.
  * Copyright (c) 2015 Jonathan Yaniv and Nitsan Bracha . All rights reserved.
  */
-public class RunnableDownloader implements Runnable {
-    private final String tempPath;
+public class RunnableDownloader implements Runnable  {
     private final URL downloadUrl;
+    private static String tempFilePrefix = "Crawler_";
+    private static String tempFileSuffixs = ".html";
 
-    public RunnableDownloader(String tempPath, URL downloadUrl) {
-        this.tempPath = tempPath;
+    public RunnableDownloader(URL downloadUrl) {
         this.downloadUrl = downloadUrl;
     }
 
@@ -26,19 +25,20 @@ public class RunnableDownloader implements Runnable {
         String line;
 
         try {
+            File tempFile = File.createTempFile(tempFilePrefix, tempFileSuffixs);  // throws an IOException
             stream = downloadUrl.openStream();  // throws an IOException
             buffer = new BufferedReader(new InputStreamReader(stream));
+            FileUtills.writeToFile(tempFile, buffer);
 
-            while ((line = buffer.readLine()) != null) {
-                //TODO: finish implementing when Analyzers ready
-                System.out.println(line);
-            }
-        } catch (IOException e) {
+        } catch (MalformedURLException e) {
             e.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         } finally {
             try {
                 if (stream != null) stream.close();
-            } catch (IOException ignored) {
+            } catch (IOException e) {
+                // Left empty
             }
         }
 
