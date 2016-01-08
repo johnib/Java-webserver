93b07dfb5baa553408a5b6ace551f9d2cc000743
Jonathan Rubin Yaniv <johnitravolta@gmail.com>
2016-01-08 22:46:07 +0200

    Code reivewed. minor fixes.

--- src/RunnableDownloader.java
+++ src/RunnableDownloader.java
@@ -2,14 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
  * Created by Jonathan Rubin Yaniv and Nitsan Bracha on 08/01/2016.
  * Copyright (c) 2015 Jonathan Yaniv and Nitsan Bracha . All rights reserved.
  */
-public class RunnableDownloader implements Runnable  {
+public class RunnableDownloader implements Runnable {
     private final String tempPath;
     private final URL downloadUrl;
 
@@ -31,17 +30,15 @@ public class RunnableDownloader implements Runnable  {
             buffer = new BufferedReader(new InputStreamReader(stream));
 
             while ((line = buffer.readLine()) != null) {
+                //TODO: finish implementing when Analyzers ready
                 System.out.println(line);
             }
-        } catch (MalformedURLException mue) {
-            mue.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         } finally {
             try {
                 if (stream != null) stream.close();
-            } catch (IOException ioe) {
-                // nothing to see here
+            } catch (IOException ignored) {
             }
         }
 
