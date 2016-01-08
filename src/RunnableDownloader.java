977b95a5c3b510ff5b6baf67b0e37ab067350a92
Nitsan Bracha <nitsi.br@gmail.com>
2016-01-08 22:18:25 +0200

    Adding class RunnableDownloader

---
+++ src/RunnableDownloader.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jonathan Rubin Yaniv and Nitsan Bracha on 08/01/2016.
+ * Copyright (c) 2015 Jonathan Yaniv and Nitsan Bracha . All rights reserved.
+ */
+public class RunnableDownloader implements Runnable  {
+    @Override
+    public void run() {
+
+    }
+}
